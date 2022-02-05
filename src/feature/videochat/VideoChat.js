aa66d4d6dcc34bd33b0efa149428f1619a457410
seodongsu <xl445566@naver.com>
2022-02-05 16:22:24 +0900

    feat: Add videochat sample

--- src/feature/videochat/VideoChat.js
+++ src/feature/videochat/VideoChat.js
@@ -1,40 +1,116 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import MainHeader from "../Rooms/MainHeader";
+import Peer from "peerjs";
 
 const VideoChat = () => {
+  const [peerId, setPeerId] = useState("");
+  const [remotePeerId, setRemotePeerId] = useState("");
+  const myVideoRef = useRef(null);
+  const remoteVideoRef = useRef(null);
+  const peerInstance = useRef(null);
+
+  useEffect(() => {
+    const peer = new Peer();
+    const getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+
+    peer.on("open", (id) => setPeerId(id));
+
+    peer.on("call", (call) => {
+      getUserMedia({ video: true }, (myStream) => {
+        myVideoRef.current.srcObject = myStream;
+
+        call.answer(myStream);
+
+        call.on("stream", (remoteStream) => {
+          remoteVideoRef.current.srcObject = remoteStream;
+        });
+      });
+    });
+
+    peerInstance.current = peer;
+  }, []);
+
+  const call = (remotePeerId) => {
+    const getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+
+    getUserMedia(
+      { video: true },
+      (myStream) => {
+        // call 버튼을 누르면 상대방에게 내 스트림을 전송하며 call을 발생시킵니다.
+        const call = peerInstance.current.call(remotePeerId, myStream);
+
+        myVideoRef.current.srcObject = myStream;
+
+        // 상대방에게 stream을 받으면 상대방 비디오에 stream을 할당 합니다.
+        call.on("stream", (remoteStream) => {
+          remoteVideoRef.current.srcObject = remoteStream;
+        });
+      },
+      (err) => {
+        console.log("Failed to get local stream", err);
+      }
+    );
+  };
+
   return (
-    <VideoChatContainer>
-      <MainHeader />
-      <VideoWrapper>
-        <div>
-          <video autoPlay playsInline />
-          <span>한소영</span>
-        </div>
-        <div>
-          <video autoPlay playsInline />
-          <span>한소영</span>
-        </div>
-        <div>
-          <video autoPlay playsInline />
-          <span>한소영</span>
-        </div>
-        <div>
-          <video autoPlay playsInline />
-          <span>한소영</span>
-        </div>
-      </VideoWrapper>
-      <EmojiWrapper>
-        <EmojiButton />
-        <EmojiButton />
-        <EmojiButton />
-        <EmojiButton />
-        <EmojiButton />
-        <EmojiButton />
-        <EmojiButton />
-        <EmojiButton />
-      </EmojiWrapper>
-    </VideoChatContainer>
+    <>
+      <h1>{peerId}</h1>
+      <p>
+        상대방 Peer ID를
+        <input
+          type="text"
+          onChange={(e) => {
+            setRemotePeerId(e.target.value);
+          }}
+        />
+        여기 사이에 입력 해주세요.
+      </p>
+      <button
+        onClick={() => {
+          call(remotePeerId);
+        }}
+      >
+        Call
+      </button>
+      <VideoChatContainer>
+        <MainHeader />
+        <VideoWrapper>
+          <div>
+            <video autoPlay playsInline ref={myVideoRef} />
+            <span>나</span>
+          </div>
+          <div>
+            <video autoPlay playsInline ref={remoteVideoRef} />
+            <span>상대방</span>
+          </div>
+          <div>
+            <video autoPlay playsInline />
+            <span>한소영</span>
+          </div>
+          <div>
+            <video autoPlay playsInline />
+            <span>한소영</span>
+          </div>
+        </VideoWrapper>
+        <EmojiWrapper>
+          <EmojiButton />
+          <EmojiButton />
+          <EmojiButton />
+          <EmojiButton />
+          <EmojiButton />
+          <EmojiButton />
+          <EmojiButton />
+          <EmojiButton />
+        </EmojiWrapper>
+      </VideoChatContainer>
+    </>
   );
 };
 
